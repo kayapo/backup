25f737f9a1d5a55aba8799b26713aaf764ef8f7b
kayapo <kayapo@khali.localdomain>
2013-05-28 08:25:14 +0200

    Initial commit

---
+++ usr/lib/backup/run.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Minden hiba vegzetes hiba, amire kilepunk
+set -e
+
+SRCDIR=`dirname $0`
+
+# Elso dolgunk betolteni a configot
+source /etc/default/backup
+
+# Ellenorizzuk, hogy letezik-e a pidfile
+if [ -f $BACKUPPID ]; then
+	# Ha igen megnezzuk fut-e a script
+	if [ `kill -0 $(cat $BACKUPPID) 2>/dev/null` ]; then
+		# Ha fut a script kilepunk hibaval
+		echo "A mentes fut:" 1>&2
+		echo "`ps -p $(cat $BACKUPPID) u`" 1>&2
+		exit 1
+	else
+		rm "$BACKUPPID" 2>&1>/dev/null
+	fi
+fi
+
+# Megcsinaljuk a pidfile -t
+echo $$>"$BACKUPPID"
+
+# A futasi konyvtarakat ellenorizzuk
+# es ha nincsennek meg akkor megcsinaljuk azokat
+[ -d "${BACKUPVAR}" ] || mkdir -p "${BACKUPVAR}"
+
+# Betoltjuk a mentes elott futtatando
+# fuggvenyeket minden fuggvenynek egyedi
+# nevvel kell rendelkeznie
+BACKUPSCRIPTS="backupnum, $BACKUPSCRIPTS"
+for fv in $(echo $BACKUPSCRIPTS | tr "," "\n")
+	do
+	. "$SRCDIR/include/$fv" && $fv pre
+	done
+
+# Indul a mentes
+/usr/bin/rsync -aSHWR --stats --delete-after $BACKUPDIRS "${BACKUPSRV}::${BACKUPTARGET}/0/"
+
+# Miutan elkeszult a mentes takaritunk magunk utan
+for fv in $(echo $BACKUPSCRIPTS | tr "," "\n")
+	do
+	$fv post
+	done
+
+# A futas vegen eltavolitjuk 
+# pidfileunkat
+rm "$BACKUPPID"
+
+exit 0
+
